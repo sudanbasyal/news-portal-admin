eca1be50a391d1c259c1c3b6cf0f24651b4478c5
Sudan Basyal <sudanbasyal74@gmail.com>
2024-11-23 21:08:02 +0545

    fixed race condition error

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,25 +1,11 @@
-"use client";
-import { useRouter } from "next/navigation";
 import AuthWrapper from "../layout/AuthWrapper";
 import DashboardLayout from "../layout/DashboardLayout/DashboardLayout";
-import { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Add a small delay or use redirect() instead
-    const redirect = setTimeout(() => {
-      router.push("/dashboard/articles");
-    }, 0);
-
-    return () => clearTimeout(redirect); // Cleanup timeout
-  }, [router]);
-
   // Return null instead of a div that will be immediately unmounted
 
   return (
